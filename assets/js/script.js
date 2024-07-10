f31f7809eb4e1f0f07d28cf9105b3ae231eae428
Kennedy Ngugi <ngugikennedy4@gmail.com>
2024-07-10 12:18:22 +0300

    Chart section introduction on the site

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+     const options = {
+         root: null,
+         rootMargin: '0px',
+         threshold: 0.1
+     };
+ 
+     const callback = (entries, observer) => {
+         entries.forEach(entry => {
+             if (entry.isIntersecting) {
+                 entry.target.classList.add('in-view');
+                 observer.unobserve(entry.target);
+             }
+         });
+     };
+ 
+     const observer = new IntersectionObserver(callback, options);
+ 
+     const targets = document.querySelectorAll('.image-container-potrait, .description');
+     targets.forEach(target => observer.observe(target));
+ });
