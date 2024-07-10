f4674e381e627b3e1cfb9fa3fb7b8c279b83c251
Kennedy Ngugi <ngugikennedy4@gmail.com>
2024-07-10 12:53:29 +0300

    Footer update

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
  
      const observer = new IntersectionObserver(callback, options);
  
-     const targets = document.querySelectorAll('.image-container-potrait, .description');
+     // const targets = document.querySelectorAll('.image-container-potrait, .description, ..chart-content-description-mofo, .chart-container');
+     const targets = document.querySelectorAll('.image-container-potrait, .description, .chart-content-description-mofo, .chart-container');
      targets.forEach(target => observer.observe(target));
  });
